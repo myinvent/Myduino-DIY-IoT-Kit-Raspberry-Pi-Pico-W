6193ea86b87d64507c5958f08568163970b41edf
ariffinzulkifli <ariffinastute@live.com>
2022-11-06 11:00:33 +0800

    update lab 5

--- Lab 5 Digital Input Pushbutton.py
+++ Lab 5 Digital Input Pushbutton.py
@@ -2,7 +2,7 @@ import time
 import board
 import digitalio
 
-pushbutton = digitalio.DigitalInOut(board.GP15)
+pushbutton = digitalio.DigitalInOut(board.GP16)
 pushbutton.direction = digitalio.Direction.INPUT
 pushbutton.pull = digitalio.Pull.DOWN
 
@@ -16,3 +16,5 @@ while True:
         led.value = True
     else:
         led.value = False
+    
+    time.sleep(0.1)
