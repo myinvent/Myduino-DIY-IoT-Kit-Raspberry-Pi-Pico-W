a9bb1d871bacc980083d0645b9244e459f13ddd5
ariffinzulkifli <ariffinastute@live.com>
2022-11-08 17:26:43 +0800

    update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -17,6 +17,7 @@ def disconnect(mqtt_client, userdata, rc):
 def subscribe(mqtt_client, userdata, topic, granted_qos):
     # This method is called when the mqtt_client subscribes to a new feed.
     print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
+    print()
 
 
 def unsubscribe(mqtt_client, userdata, topic, pid):
@@ -26,9 +27,4 @@ def unsubscribe(mqtt_client, userdata, topic, pid):
 
 def publish(mqtt_client, userdata, topic, pid):
     # This method is called when the mqtt_client publishes data to a feed.
-    print("Published to {0} with PID {1}".format(topic, pid))
-
-
-def message(client, topic, message):
-    # Method called when a client's subscribed feed has a new value.
-    print("New message on topic {0}: {1}".format(topic, message))
+    print("Data published to {0}".format(topic))
