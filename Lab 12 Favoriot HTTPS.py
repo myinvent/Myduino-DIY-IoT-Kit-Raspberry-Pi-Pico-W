6918849bfa5db5e9392d4dd8bab6f1a335851a2f
ariffinzulkifli <ariffinastute@live.com>
2022-11-08 14:22:14 +0800

    added lab 12

---
+++ Lab 12 Favoriot HTTPS.py
@@ -0,0 +1,91 @@
+import os
+import time
+import board
+import ssl
+import wifi
+import socketpool
+import microcontroller
+import adafruit_requests as requests
+import adafruit_dht
+import favoriot_ca
+
+dht11 = adafruit_dht.DHT11(board.GP15)
+
+ssid = os.getenv('WIFI_SSID')
+password = os.getenv('WIFI_PASSWORD')
+
+print("""
+    ______                       _       __ 
+   / ____/___ __   ______  _____(_)___  / /_
+  / /_  / __ `/ | / / __ \/ ___/ / __ \/ __/
+ / __/ / /_/ /| |/ / /_/ / /  / / /_/ / /_  
+/_/    \__,_/ |___/\____/_/  /_/\____/\__/ v3.0.0""" + " (Microcontroller: " + os.uname()[0] + ")\n")
+
+print("Connecting to '" + ssid + "' ... ", end="")
+
+wifi.radio.connect(ssid, password)
+
+print("connected.")
+print()
+
+pool = socketpool.SocketPool(wifi.radio)
+context = ssl.create_default_context()
+context.load_verify_locations(cadata=favoriot_ca.cert)
+https = requests.Session(pool, context)
+
+now = time.monotonic()
+
+while True:
+    
+    try:
+        temperature = dht11.temperature
+        humidity = dht11.humidity
+        
+        print("Temperature: {} °C, Humidity: {} %RH ".format(temperature, humidity))
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+    
+    time.sleep(2)
+    
+    if (now + 15) < time.monotonic():
+        
+        now = time.monotonic()
+    
+        headers = {
+            "content-type": "application/json",
+            "apikey": os.getenv('FAVORIOT_DEVICE_ACCESS_TOKEN')
+        }
+        
+        json = {
+            "device_developer_id": os.getenv('FAVORIOT_DEVICE_DEVELOPER_ID'),
+            "data": {
+                "temperature": temperature,
+                "humidity": humidity
+            }
+        }
+
+        request = https.request(
+            "POST",
+            "https://apiv2.favoriot.com/v2/streams",
+            timeout = 5,
+            headers = headers,
+            json = json
+        )
+        
+        response = request.json()
+        print("Favoriot HTTPS Request: ", end="")
+
+        if response["statusCode"] == 20150:
+            print("Success: " + response["message"])
+        else:
+            print("Error: " + response["message"])
+                
+        request.close()
+        
+        print()
