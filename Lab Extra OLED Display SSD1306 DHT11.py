2fa2427a7f02751993e2911c00c767b100092f34
ariffinzulkifli <ariffinastute@live.com>
2022-12-08 16:12:21 +0800

    update sketch

---
+++ Lab Extra OLED Display SSD1306 DHT11.py
@@ -0,0 +1,52 @@
+import time
+import board
+import busio
+import displayio
+import terminalio
+from adafruit_display_text import label
+import adafruit_displayio_ssd1306
+import adafruit_dht
+
+displayio.release_displays()
+
+i2c_oled = busio.I2C(scl=board.GP5, sda=board.GP4)
+oled_bus = displayio.I2CDisplay(i2c_oled, device_address=0x3C)
+oled = adafruit_displayio_ssd1306.SSD1306(oled_bus, width=128, height=64)
+
+dht11 = adafruit_dht.DHT11(board.GP15)
+
+while True:
+    try:
+        temperature = dht11.temperature
+        humidity = dht11.humidity
+        
+        print("Temperature: {} °C, Humidity: {} %RH ".format(temperature, humidity))
+        
+        screen = displayio.Group()
+        
+        text = "DHT11 SENSOR"
+        text_font = terminalio.FONT
+        text_color = 0xFFFFFF
+
+        text_area = label.Label(text_font, text=text, color=text_color, x=0, y=10)
+        screen.append(text_area)
+        
+        text = "Temperature: {} *C".format(temperature)
+        text_area = label.Label(text_font, text=text, color=text_color, x=0, y=25)
+        screen.append(text_area)
+
+        text = "Humidity   : {} %RH".format(humidity)
+        text_area = label.Label(text_font, text=text, color=text_color, x=0, y=37)
+        screen.append(text_area)
+        
+        oled.show(screen)
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dht11.exit()
+        raise error
+
+    time.sleep(2.0)
--- Lab Extra OLED Display SSD1306.py
+++ Lab Extra OLED Display SSD1306.py
@@ -7,25 +7,21 @@ import adafruit_displayio_ssd1306
 
 displayio.release_displays()
 
-i2c = busio.I2C(scl=board.GP5, sda=board.GP4)
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+i2c_oled = busio.I2C(scl=board.GP5, sda=board.GP4)
+oled_bus = displayio.I2CDisplay(i2c_oled, device_address=0x3C)
+oled = adafruit_displayio_ssd1306.SSD1306(oled_bus, width=128, height=64)
 
-# Make the display context
 screen = displayio.Group()
-display.show(screen)
 
 # Hello World!
 text = "Hello World!"
 text_font = terminalio.FONT
 text_color = 0xFFFFFF
 
-text_area = label.Label(text_font, text=text, color=text_color)
-
-text_area.x = 28
-text_area.y = 15
-
+text_area = label.Label(text_font, text=text, color=text_color, x=28, y=15)
 screen.append(text_area)
+
+oled.show(screen)
 
 while True:
     pass
