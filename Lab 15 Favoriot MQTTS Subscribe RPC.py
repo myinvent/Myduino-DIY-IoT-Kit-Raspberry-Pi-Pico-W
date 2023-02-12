c643ad9c5b4cb6fe19d8fa21174ba1a90b80fc77
ariffinzulkifli <ariffinastute@live.com>
2023-02-12 15:34:42 +0800

    update sketch

--- Lab 15 Favoriot MQTTS Subscribe RPC.py
+++ Lab 15 Favoriot MQTTS Subscribe RPC.py
@@ -64,7 +64,7 @@ def mqtt_message(client, topic, message):
 # Set up a MiniMQTT Client
 mqtt_client = MQTT.MQTT(
     broker=os.getenv("FAVORIOT_MQTT_BROKER_HOST"),
-    port=os.getenv("FAVORIOT_MQTT_BROKER_PORT"),
+    port=os.getenv("FAVORIOT_MQTT_BROKER_SECURE_PORT"),
     username=os.getenv("FAVORIOT_DEVICE_ACCESS_TOKEN"),
     password=os.getenv("FAVORIOT_DEVICE_ACCESS_TOKEN"),
     socket_pool=pool,
